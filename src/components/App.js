0a27c84691e623fa129fbd8fa5e90c2375c39c0a
Wiesław Bikowski <wieslaw.botak@gmail.com>
2021-03-03 14:33:52 +0100

    Added bg images and animations on main page

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,27 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "../styles/App.css";
 import Header from "./Header";
 import Main from "./Main";
+import $ from "jquery";
 
 class App extends Component {
   componentDidMount() {
-    document.querySelector(".menuIcon").addEventListener("click", () => {
-      document.querySelector(".headerNavigation").classList.toggle("active");
+    //handle menu icon and option click
+    $(".menuIcon").on("click", () => {
+      $(".headerNavigation").toggleClass("active");
     });
 
-    document.querySelectorAll("a").forEach((item) => {
-      item.addEventListener("click", () => {
-        document.querySelector(".headerNavigation").classList.toggle("active");
+    $("a").each(function (item) {
+      $(this).on("click", () => {
+        $(".headerNavigation").toggleClass("active");
+      });
+    });
+
+    //show elements on scroll
+    $(window).on("scroll", function () {
+      let scrollPosition = $(this).scrollTop();
+      $("div.content").each(function (item) {
+        let itemOffsetTop = $(this).offset().top;
+        if (itemOffsetTop - 600 < scrollPosition) $(this).addClass("active");
       });
     });
   }
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,20 +5,34 @@ const Home = () => {
   return (
     <>
       <section className="introduction">
+        <div className="backgroundImage"></div>
+        <div className="fadeAway"></div>
         <div className="content">
           <h4>Kim jesteśmy?</h4>
           <p>
             Meblex jest firmą, która od 20 lat zaopatruje naszych klijentów we
             wszelkiego rodzaju meble kuchenne, łazienkowe oraz wszystko to co
             umieścić można w salonie. Jesteśmy również doradcami, dbającymi o
-            to, by każdy zakątek domu jak najlepiej odzwierciedlał twoje
+            to, by każdy zakątek domu jak najlepiej odzwierciedlał Twoje
             oczekiwania.
           </p>
         </div>
       </section>
       <section className="offerts">
+        <div className="backgroundImage"></div>
         <div className="content">
           <h4>Nasza oferta</h4>
+          <p>
+            Oferujemy produkty wykonane z dbałością oraz z materiałów najlepszej
+            jakości. Nasi pracownicy pomogą Ci w odpowiednim zorganizowaniu
+            zakupów, dostarczą produkt do domu oraz pomogą w jego montarzu.
+          </p>
+        </div>
+      </section>
+      <section className="showcase">
+        <div className="backgroundImage"></div>
+        <div className="content">
+          <h4>Lorem </h4>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
             est enim, ipsa laudantium atque debitis fuga iusto quod laborum
