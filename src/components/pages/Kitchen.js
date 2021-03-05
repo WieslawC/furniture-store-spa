9fb3446d23dc1d290701241d9219d24224e343e1
Wiesław Bikowski <wieslaw.botak@gmail.com>
2021-03-05 15:42:03 +0100

    Fixed menu icon on large devices, started /kitchen

--- src/components/pages/Kitchen.js
+++ src/components/pages/Kitchen.js
@@ -1,7 +1,25 @@
 import React from "react";
+import "../../styles/Kitchen.scss";
 
 const Kitchen = () => {
-  return <p>Kitchen</p>;
+  return (
+    <>
+      <div className="mainKitchen">
+        <div className="kitchenSliderWrapper">
+          <h4>Kuchnia twoich marzeń</h4>
+          <section className="kitchenSlider"></section>
+        </div>
+        <div className="kitchenAbout">
+          <h4>Informacje</h4>
+          <section className="kitchenSlider"></section>
+        </div>
+        <div className="kitchenContent">
+          <h4>Treść</h4>
+          <section className="kitchenSlider"></section>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Kitchen;
